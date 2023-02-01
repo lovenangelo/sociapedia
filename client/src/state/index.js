6a964602eff0f4e0f8e7a817e0d9641d1afdfd42
lovenangelo <1901102159@student.buksu.edu.ph>
2023-02-01 08:32:31 +0800

    working with navbar

---
+++ client/src/state/index.js
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("user friends non-existent");
+      }
+    },
+    setPosts: (state, action) => {
+      if (action.payload.posts.length !== 0) {
+        state.posts = action.posts;
+      }
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post_id) {
+          return action.payload.post;
+        }
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+
+export default authSlice.reducer;

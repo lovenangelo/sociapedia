cea2e3f1817d69a2aa6d1ea371c2f7aea60b89c1
lovenangelo <1901102159@student.buksu.edu.ph>
2023-02-03 19:18:57 +0800

    auth forms

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,34 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 768px)");
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociapedia
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Sociapedia, the Social Media for Social People!
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
-  const isNonMobileScreens = useMediaQuery("(min-width: 100px)");
+  const isNonMobileScreens = useMediaQuery("(min-width: 768px)");
 
   const theme = useTheme();
   const neutralLight = theme.palette.neutral.light;
@@ -39,7 +39,8 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.default;
   const alt = theme.palette.background.alt;
 
-  const fullName = `${user.firstName} ${user.lastName}`;
+  // const fullName = `${user.firstName} ${user.lastName}`;
+  const fullName = "Loven Angelo Dayola";
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
